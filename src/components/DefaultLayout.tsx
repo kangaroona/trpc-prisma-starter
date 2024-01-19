ae0de2041994ca69ea3e5304c89b3f7236d84243
kangaroona <79397550@qq.com>
2024-01-18 17:33:39 -0800

    fix: script load order fix bug

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -6,18 +6,21 @@ const spyConfig = { title: 'tprcnext' };
 export const DefaultLayout = ({ children }: DefaultLayoutProps) => {
   useInsertionEffect(() => {
     const script = document.createElement('script');
+    const script1 = document.createElement('script');
     script.setAttribute(
       'src',
       'http://192.168.1.81:6752/page-spy/index.min.js',
     );
     script.setAttribute('crossOrigin', 'anonymous');
+    script.onload = () => {
+      script1.innerHTML = 'window.$pageSpy = new PageSpy({"title":"123"});';
+      document.body.appendChild(script1);
+    };
     document.body.appendChild(script);
-    const script1 = document.createElement('script');
-    script1.innerHTML = 'window.$pageSpy = new PageSpy({"name":"123"});';
-    document.body.appendChild(script1);
+
     return () => {
       document.body.removeChild(script);
-      document.body.removeChild(script1);
+      script1 && document.body.removeChild(script1);
     };
   }, []);
   return (
